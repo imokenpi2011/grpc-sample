c3283d770803e367b901fd54c4529ef048cd32a3
anthem-landing18 <imokenpi2011@gmail.com>
2021-12-02 00:58:58 +0900

    チャットサービスを追加したサーバ起動まで実装

--- Server.go
+++ Server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+
+	"grpc-sample/chat"
 
 	"google.golang.org/grpc"
 )
@@ -13,13 +16,23 @@ const (
 
 func main() {
 
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
 	// 通信方式を指定
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	// chatサービスの定義
+	s := chat.Server{}
+
 	// サーバを起動
 	grpcServer := grpc.NewServer()
+
+	// サーバにchatサービスを登録
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
